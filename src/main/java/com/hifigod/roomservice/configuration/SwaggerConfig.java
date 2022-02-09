d30490de8b88d3229f9d6fb4fd5cb5e223129d2b
Parakramaba <p.vitharana.pv@gmail.com>
2022-02-09 10:02:00 +0530

    [Config] : Swagger configuration

---
+++ src/main/java/com/hifigod/roomservice/configuration/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.hifigod.roomservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket roomApi() {
+        return new Docket(DocumentationType.SWAGGER_2).
+                select().
+                paths(PathSelectors.ant("/api/rooms/*")).
+                apis(RequestHandlerSelectors.basePackage("com.hifigod.roomservice")).
+                build().
+                apiInfo(roomApiInfo());
+    }
+
+    private ApiInfo roomApiInfo() {
+        return new ApiInfo(
+                "HiFiGod - Room Service",
+                "API for Room Service",
+                "1.0.0",
+                "",
+                new springfox.documentation.service.Contact("HiFiGod", "", ""),
+                "API License",
+                "",
+                Collections.emptyList()
+        );
+    }
+}
